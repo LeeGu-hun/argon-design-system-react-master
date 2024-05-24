82cfa276ef8efd91fe0ce2d403c9acefa7ff9773
ihd12 <ihd+1@enbics.com>
2024-05-24 15:24:17 +0900

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*!
 
 =========================================================
-* Argon Design System React - v1.1.2
+* Argon Design System React -  v1.1.2
 =========================================================
 
 * Product Page: https://www.creative-tim.com/product/argon-design-system-react
@@ -15,26 +15,26 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
 
-import "assets/vendor/nucleo/css/nucleo.css";
-import "assets/vendor/font-awesome/css/font-awesome.min.css";
-import "assets/scss/argon-design-system-react.scss?v1.1.0";
+import 'assets/vendor/nucleo/css/nucleo.css'
+import 'assets/vendor/font-awesome/css/font-awesome.min.css'
+import 'assets/scss/argon-design-system-react.scss?v1.1.0'
 
 // import Index from "views/Index.js";
-import Landing from "views/examples/Landing.js";
-import CarApplication from "views/examples/CarApplication.js";
-import CarInquiry from "views/examples/CarInquiry.js";
-import CarRegist from "views/examples/CarRegist.js";
-import MemInquiry from "views/examples/MemInquiry.js";
-import MemRegist from "views/examples/MemRegist.js";
-import Login from "views/examples/Login.js";
-import Profile from "views/examples/Profile.js";
-import Register from "views/examples/Register.js";
+import Landing from 'views/examples/Landing.js'
+import CarApplication from 'views/examples/CarApplication.js'
+import CarInquiry from 'views/examples/CarInquiry.js'
+import CarRegist from 'views/examples/CarRegist.js'
+import MemInquiry from 'views/examples/MemInquiry.js'
+import MemRegist from 'views/examples/MemRegist.js'
+import Login from 'views/examples/Login.js'
+import Profile from 'views/examples/Profile.js'
+import Register from 'views/examples/Register.js'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <BrowserRouter>
@@ -53,4 +53,4 @@ root.render(
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   </BrowserRouter>
-);
+)
